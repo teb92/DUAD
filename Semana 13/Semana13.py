ddbbc051461276ef675073233d7c6955d8e9eb63
teb92 <147997027+teb92@users.noreply.github.com>
2025-01-30 22:41:36 -0600

    Ejercicios de Decoradores

---
+++ Semana 13/Semana13.py
@@ -0,0 +1,78 @@
+
+# 💪🏽 **Ejercicios**
+
+# 1. Cree un decorador que haga print de los parámetros y retorno de la función que decore.
+
+def print_info(funcion):
+    def wrapper(x, y):
+        print(f"Parameters to use: {x} y {y}")
+        result = funcion(x, y)
+        print(f"El result is: {result}")
+        return result
+    return wrapper
+
+@print_info
+def sum(a, b):
+    return a + b
+
+sum(3, 5)
+
+# 2. Cree un decorador que se encargue de revisar si todos los parámetros de la 
+# función que decore son números, y arroje una excepción de no ser así.
+
+def validate_numbers(func):
+    def wrapper(a, b):
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return func(a, b)
+        else:
+            return "Both arguments must be numbers"
+    return wrapper
+
+@validate_numbers
+def sum(a, b):
+    return a + b
+
+print(sum(3, 5))  
+print(sum(3, "five")) 
+
+# 3. Cree una clase de `User` que:
+#     - Tenga un atributo de `date_of_birth`.
+#     - Tenga un property de `age`.
+    
+#     Luego cree un decorador para funciones que acepten un `User` como parámetro que se
+# encargue de revisar si el `User` es mayor de edad y arroje una excepción de no ser así.
+
+from datetime import date
+
+
+class User:
+    def __init__(self, date_of_birth):
+        self.date_of_birth = date_of_birth
+
+    @property
+    def age(self):
+        today = date.today()
+        return today.year - self.date_of_birth.year - (
+            (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day)
+        )
+
+
+def check_adult_user(func):
+    def wrapper(user):
+        if user.age > 18:
+            return func(user)
+        else:
+            print(f"User with age {user.age} is not an adult")
+    return wrapper
+
+
+@check_adult_user
+def greet_user(user):
+    print(f"Hello, User! You are {user.age} years old.")
+
+
+user1 = User(date(2010, 1, 1))  
+adult_user = User(date(1990, 6, 15)) 
+
+greet_user(user1)  
+greet_user(adult_user) 
